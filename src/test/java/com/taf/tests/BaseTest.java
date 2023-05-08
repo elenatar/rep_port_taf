6c2f26b1f994cb4a5fc0d2e90653372dabe59d16
Elena Tarnopolska <Elena_Tarnopolska@epam.com>
2023-05-08 10:37:23 +0200

    Fixes

--- src/test/java/com/taf/tests/BaseTest.java
+++ src/test/java/com/taf/tests/BaseTest.java
@@ -4,16 +4,21 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.taf.core.ConfigManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.open;
 
 class BaseTest {
 
-    @BeforeEach
-    public void setUp() {
+    @BeforeAll
+    public static void setUp() {
         Configuration.browser = "chrome";
         Configuration.baseUrl = ConfigManager.getUrl();
+    }
+
+    @BeforeEach
+    public void openBaseURL() {
         open("/");
     }
 
